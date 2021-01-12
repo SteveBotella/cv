be89c516ced65be5e9c9db837f3fcecfca6af99d
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-12 13:55:53 +0100

    Création du dossier contact et du fichier text Ymdhis.txt

--- pages/contact.php
+++ pages/contact.php
@@ -7,6 +7,14 @@ require 'header.php';
         <div class="Ou"><p>Ou remplir le formulaire de contact ci-dessous</p></div>
         <!--Le Formulaire super compliqué de la mort qui tue tout-->
         <form action="../index.php?page=contact" method="post">
+            <div class="Civilite">
+                <label for="civilite-select">Mr ou Mme</label>
+                <select name="civilite" id="civilite-select">
+                    <option value="">--Choisir une civilite--</option>
+                    <option value="Mr">Monsieur</option>
+                    <option value="Mme">Madame</option>
+                </select>
+            </div>
             <div class="Name">
                 <label class="NameSpace" for="name">Nom :</label>
                 <input type="text" id="name" name="user_name">
@@ -16,17 +24,16 @@ require 'header.php';
                 <input type="email" id="mail" name="user_mail">
             </div>
             <div class="Contact">
-                <label for="Raison-select">En quoi puis-je vous aider ?:</label>
-                <select name="Raison" id="Raison-select">
-                    <option value="">--Choisir une raison--</option>
-                    <option value="question">Juste une question</option>
-                    <option value="Contrat">Contrat CDD/CDI</option>
-                    <option value="Rencontre">Me rencontrer</option>
-                </select>
+                <label for="question">Juste une question</label>
+                <input type="radio" id="question" name="raison-contact" value="question">
+                <label for="Contrat">Contrat CDD/CDI</label>
+                <input type="radio" id="Contrat" name="raison-contact" value="Contrat">
+                <label for="Rencontre">Me rencontrer</label>
+                <input type="radio" id="Rencontre" name="raison-contact" value="Rencontre">
             </div>
             <div class="MP">
                 <label for="msg">Message :</label>
-                <textarea id="msg" name="user_message" placeholder="Votre message" required></textarea>
+                <textarea id="msg" name="user_message" placeholder="Votre message"></textarea>
                 <input class="BouttonEnvoyer" type="submit" value="Envoyer">
                 <input class="BouttonReset" type="reset" value="Reset">
             </div>
