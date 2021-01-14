7bf2316543f91a2d7cc9d6f9c5d9db74e21a9c33
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-14 16:38:18 +0100

    Ajout des éléments session avec compteur et date

--- index.php
+++ index.php
@@ -1,20 +1,25 @@
 <?php
 $_SESSION['dateFirstVisit'] = date('_Y-m-d-H-i-s');
+$_SESSION['countViewPage'] = 1;
 $raw_url = filter_input(INPUT_GET, 'page', FILTER_SANITIZE_STRING);
 if (isset($raw_url)) {
     switch ($raw_url) {
         case 'hobby':
             include 'pages/hobby.php';
+            $_SESSION['countViewPage'] = $_SESSION['countViewPage'] + 1;
             break;
         case 'contact':
             include 'pages/contact.php';
+            $_SESSION['countViewPage'] = $_SESSION['countViewPage'] + 1;
             break;
         case 'cv':
             include 'pages/cv.php';
+            $_SESSION['countViewPage'] = $_SESSION['countViewPage'] + 1;
             break;
         default:
             include 'pages/404.php';
     }
 } else {
     include 'pages/cv.php';
+    $_SESSION['countViewPage'] = $_SESSION['countViewPage'] + 1;
 }
--- pages/footer.php
+++ pages/footer.php
@@ -1,7 +1,7 @@
 <footer>
     <p>Copyright Steve 2021 - (Last update : 07/01/2021)</p>
     <?php
-    echo $_SESSION['dateFirstVisit'];
+    echo $_SESSION['dateFirstVisit'] . $_SESSION['countViewPage'];
     ?>
 </footer>
 </body>
