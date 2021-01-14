e721a13558519bb2139565d16f841ce0e966fe1b
SteveBotella <steve.botella@le-campus-numerique.fr>
2021-01-14 15:19:47 +0100

    Optimisation du formulaire de la page contact.php

--- pages/contact.php
+++ pages/contact.php
@@ -1,48 +1,26 @@
 <?php
 require 'header.php';
+
 if (filter_has_var(INPUT_POST, 'valider')) {
     $message = '';
-    $civilite = $_POST['civilite'];
-    $nom = $_POST['user_name'];
-    $mail = $_POST['user_mail'];
-    if (filter_has_var(INPUT_POST, 'raison-contact')) {
-        $contact = $_POST['raison-contact'];
-    }
-    $MP = $_POST['user_message'];
+    $file_contact_civilite = filter_input(INPUT_POST, 'civilite', FILTER_DEFAULT);
+    $file_contact_name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_STRING);
+    $file_contact_email = filter_input(INPUT_POST, 'user_mail', FILTER_VALIDATE_EMAIL);
+    $file_contact_raison = filter_input(INPUT_POST, 'raison-contact', FILTER_DEFAULT);
+    $file_contact_mp = filter_input(INPUT_POST, 'user_message', FILTER_SANITIZE_STRING);
     $valider = $_POST['valider'];
-    $IsValid = false;
+    $can_send = false;
 
-    if (isset($valider)) {
-        if (empty($civilite)) {
-            $message = '<div class="Erreur">Aucun élément sélectionné.</div>';
-        } elseif (empty($nom)) {
-            $message = '<div class="Erreur">Entrer un nom.</div>';
-        } elseif (empty($mail)) {
-            $message = '<div class="Erreur">Entrer une adresse email.</div>';
-        } elseif (empty($contact)) {
-            $message = '<div class="Erreur">Choisir une raison.</div>';
-        } elseif (empty($MP)) {
-            $message = '<div class="Erreur">Entrer un message.</div>';
-        } elseif (strlen($MP) < 5) {
-            $message = '<div class="Erreur">Message trop court.</div>';
-        } elseif (empty($nom)) {
-            $message = '<div class="Erreur">Entrer un nom.</div>';
-        } else {
-            $IsValid = true;
-            $message = '<div class="Erreur">Message envoyé avec succès.</div>';
-        }
+    if (!empty($file_contact_civilite) && !empty($file_contact_name) && !empty($file_contact_email) && !empty($file_contact_raison) && !empty($file_contact_mp)) {
+        $can_send = true;
     }
-    if ($IsValid) {
+
+    if (isset($valider) && ($can_send)==true) {
         $file_contact = 'contact/contact' . date('_Y-m-d-H-i-s') . '.txt';
-        $file_contact_civilite = filter_input(INPUT_POST, 'civilite', FILTER_DEFAULT);
         file_put_contents($file_contact, $file_contact_civilite, FILE_APPEND | LOCK_EX);
-        $file_contact_name = filter_input(INPUT_POST, 'user_name', FILTER_SANITIZE_STRING);
         file_put_contents($file_contact, $file_contact_name, FILE_APPEND | LOCK_EX);
-        $file_contact_raison = filter_input(INPUT_POST, 'raison-contact', FILTER_DEFAULT);
         file_put_contents($file_contact, $file_contact_raison, FILE_APPEND | LOCK_EX);
-        $file_contact_email = filter_input(INPUT_POST, 'user_mail', FILTER_VALIDATE_EMAIL);
         file_put_contents($file_contact, $file_contact_email, FILE_APPEND | LOCK_EX);
-        $file_contact_mp = filter_input(INPUT_POST, 'user_message', FILTER_SANITIZE_STRING);
         file_put_contents($file_contact, $file_contact_mp, FILE_APPEND | LOCK_EX);
     }
 }
@@ -52,11 +30,6 @@ if (filter_has_var(INPUT_POST, 'valider')) {
         </div>
         <div class="Ou"><p>Ou remplir le formulaire de contact ci-dessous</p></div>
         <!--Le Formulaire super compliqué de la mort qui tue tout-->
-        <?php
-        if (filter_has_var(INPUT_POST, 'valider')) {
-            echo $message;
-        }
-        ?>
         <form action="../index.php?page=contact" method="post">
             <div class="Civilite">
                 <label for="civilite-select">Mr ou Mme</label>
@@ -66,14 +39,35 @@ if (filter_has_var(INPUT_POST, 'valider')) {
                     <option value="Mme">Madame</option>
                 </select>
             </div>
+            <?php
+            if (isset($valider)) {
+                if (empty($file_contact_civilite)) {
+                    echo 'Aucune civilité sélectionnée.';
+                }
+            }
+            ?>
             <div class="Name">
                 <label class="NameSpace" for="name">Nom :</label>
                 <input type="text" id="name" name="user_name">
             </div>
+            <?php
+            if (isset($valider)) {
+                if (empty($file_contact_name)) {
+                    echo 'Entrer un nom.';
+                }
+            }
+            ?>
             <div class="Mail">
                 <label for="mail">e-mail :</label>
                 <input type="email" id="mail" name="user_mail">
             </div>
+            <?php
+            if (isset($valider)) {
+                if (empty($file_contact_email)) {
+                    echo 'Entrer une adresse email.';
+                }
+            }
+            ?>
             <div class="Contact">
                 <label for="question">Juste une question</label>
                 <input type="radio" id="question" name="raison-contact" value="question">
@@ -82,9 +76,23 @@ if (filter_has_var(INPUT_POST, 'valider')) {
                 <label for="Rencontre">Me rencontrer</label>
                 <input type="radio" id="Rencontre" name="raison-contact" value="Rencontre">
             </div>
+            <?php
+            if (isset($valider)) {
+                if (empty($file_contact_raison)) {
+                    echo 'Choisir une raison.';
+                }
+            }
+            ?>
             <div class="MP">
                 <label for="msg">Message :</label>
                 <textarea id="msg" name="user_message" placeholder="Votre message"></textarea>
+                <?php
+                if (isset($valider)) {
+                    if (empty($file_contact_mp)) {
+                        echo 'Entrer un message.';
+                    }
+                }
+                ?>
                 <input class="BouttonEnvoyer" type="submit" name="valider" value="Envoyer">
                 <input class="BouttonReset" type="reset" value="Reset">
             </div>
